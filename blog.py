ca14a993ae2e37fe783153ca0e3dbcf010cb450c
CIPHERTron <pritish.samal918@gmail.com>
2021-04-10 02:38:14 +0530

    Add home as well as about decorators

--- blog.py
+++ blog.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [{
+    'author': 'John Doe',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'April 21, 2018'
+}, {
+    'author': 'Mr. X',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 10, 2018'
+}]
 
 
 @app.route('/')
 def home():
-    return '<h1>Hello, World!</h1>'
+    return render_template('home.html', posts=posts, title="Home")
+    # You can also write multiline html by using ''' </> '''
 
 
 @app.route('/about')
 def about():
-    return '<h1>About page!</h1>'
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
