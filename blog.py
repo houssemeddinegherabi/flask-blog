0df19074c946e5a8f550d21302690e32069eeb1e
CIPHERTron <pritish.samal918@gmail.com>
2021-04-11 10:34:58 +0530

    SQLAlchemy: Get rid of deprecation warning

--- blog.py
+++ blog.py
@@ -6,6 +6,7 @@ from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '68a2c99120b7e9558c126213b6989f1d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
