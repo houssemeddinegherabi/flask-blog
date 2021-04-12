f4913849ae2b9147ad9cb59526aefabaef6f67ef
CIPHERTron <pritish.samal918@gmail.com>
2021-04-12 18:29:59 +0530

    Initialize app with LoginManager

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '68a2c99120b7e9558c126213b6989f1d'
@@ -8,5 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blog import routes
