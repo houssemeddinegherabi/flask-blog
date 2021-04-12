929d51ac1fadbef2ca0e511c98ea270abf475032
CIPHERTron <pritish.samal918@gmail.com>
2021-04-12 18:25:00 +0530

    Hash Password, add email and username validators

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '68a2c99120b7e9558c126213b6989f1d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from blog import routes
