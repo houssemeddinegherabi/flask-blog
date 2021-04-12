c1677497410e8a7d4c9f1ca6bb9933586cf9b6be
CIPHERTron <pritish.samal918@gmail.com>
2021-04-12 21:56:17 +0530

    Add Post update and delete routes

--- blog/routes.py
+++ blog/routes.py
@@ -7,23 +7,15 @@ from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [{
-    'author': 'John Doe',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'April 21, 2018'
-}, {
-    'author': 'Mr. X',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'April 10, 2018'
-}]
-
 
 @app.route('/')
+@app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts, title="Home")
-    # You can also write multiline html by using ''' </> '''
+
+
+# You can also write multiline html by using ''' </> '''
 
 
 @app.route('/about')
@@ -128,3 +120,43 @@ def new_post():
                            title='New Post',
                            form=form,
                            legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',
+                           title='Update Post',
+                           form=form,
+                           legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
